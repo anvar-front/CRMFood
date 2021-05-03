547adc94dc29e0fd391aa86a5c42fa0d4afe0099
anvar-front <69399311+anvar-front@users.noreply.github.com>
2021-05-04 01:24:10 +0300

    updated admin.py in app

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Table)
+admin.site.register(Order)
+admin.site.register(ServicePercentage)
+admin.site.register(Status)
+admin.site.register(Check)
+admin.site.register(MealsToOrder)
+admin.site.register(Department)
+admin.site.register(Meal)
+admin.site.register(MealCategory)
