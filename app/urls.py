3714f17ce242617c555d11a0c8cc657508218307
anvar-front <69399311+anvar-front@users.noreply.github.com>
2021-05-04 01:23:14 +0300

    created urls.py in app and updated

---
+++ app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+  path('tables/', views.TableListView.as_view()),
+  path('departments/', views.DepartmentListView.as_view()),
+  path('categories/', views.MealCategoryListView.as_view()),
+  path('statuses/', views.StatusListView.as_view()),
+  path('percentage/', views.ServicePercentageListView.as_view()),
+  path('meals/', views.MealListView.as_view()),
+  path('meals/<int:pk>/', views.MealDetailView.as_view()),
+  path('orders/', views.OrderListView.as_view()),
+  path('checks/', views.CheckListView.as_view()),
+  path('mealstoorder/', views.MealsToOrderListView.as_view()),
+  path('activeorders/', views.ActiveOrderListView.as_view()),    
+]
+
+    
